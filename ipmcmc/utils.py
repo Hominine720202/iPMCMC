6d453dac253803a82e43a05dcfb455e17657064a
Corentin Ambroise <corentin.ambroise9132@gmail.com>
2019-12-16 00:50:18 +0100

    added gaussian density to utils

--- ipmcmc/utils.py
+++ ipmcmc/utils.py
@@ -1,2 +1,14 @@
+import numpy as np
+
 def init_proposal(sampler, params):
     return lambda n: sampler(**params, size=n)
+
+
+def gaussian_density(mu, var):
+    assert (type(mu) == np.ndarray and var.shape == (len(mu), len(mu))) or (type(float(mu)) == float and type(float(var)) == float)
+    def dens_func(x):
+        if type(mu) == np.ndarray:
+            N = len(mu)
+            return 1/np.sqrt((2*np.pi)**N * np.linalg.det(var)) * np.exp(-1/2 * np.transpose(x - mu) @ np.linalg.inv(var) @ (x - mu))
+        return 1/np.sqrt(2*np.pi * var) * np.exp(-1/(2*var) * (x - mu)**2)
+    return dens_func
